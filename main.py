8c540dcbf4eb78798fea014e45f0aa27aed3b705
huangdavid <2461619116@qq.com>
2025-09-24 22:27:15 -0400

    merge sort algo

--- main.py
+++ main.py
@@ -13,4 +13,42 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('hello world')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# merge sort algorithm implementation
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]  # Dividing the elements into 2 halves
+        R = arr[mid:]
+
+        merge_sort(L)  # Sorting the first half
+        merge_sort(R)  # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+# test merge sort algorithm
+if __name__ == '__main__':
+    arr = [38, 27, 43, 3, 9, 82, 10]
+    print("Unsorted array is:", arr)
+    merge_sort(arr)
+    print("Sorted array is:", arr)
